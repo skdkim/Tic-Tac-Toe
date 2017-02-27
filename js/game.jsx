e0c33cc03bb8d07efa5c196d1ce3f962af8d43d9
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-02-26 18:09:05 -0800

    update styling

--- js/game.jsx
+++ js/game.jsx
@@ -64,16 +64,18 @@ class Game extends React.Component {
     });
 
     return (
-      <div className="game">
-        <div>
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+      <div className="bg">
+        <div className="game">
+          <div>
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+              />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
       </div>
     );
